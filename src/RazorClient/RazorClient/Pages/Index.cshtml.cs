8d1d184182578df765b0e85a08977daa0dea79ef
Amir <amirmahdi13579@gmail.com>
2024-05-17 17:30:08 +0300

    добавил для теста селект файла

--- src/RazorClient/RazorClient/Pages/Index.cshtml.cs
+++ src/RazorClient/RazorClient/Pages/Index.cshtml.cs
@@ -1,18 +1,32 @@
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using IHostingEnvironment = Microsoft.AspNetCore.Hosting.IHostingEnvironment;
 
 namespace RazorClient.Pages;
 
 public class IndexModel : PageModel
 {
     private readonly ILogger<IndexModel> _logger;
+    private IHostingEnvironment _environment;
 
-    public IndexModel(ILogger<IndexModel> logger)
+    public IndexModel(ILogger<IndexModel> logger, IHostingEnvironment hostingEnvironment)
     {
         _logger = logger;
+        _environment = hostingEnvironment;
     }
 
     public void OnGet()
     {
+    }
+    
+    [BindProperty]
+    public IFormFile Upload { get; set; }
+    public async Task OnPostAsync()
+    {
+        var file = Path.Combine(_environment.ContentRootPath, "uploads", Upload.FileName);
+        using (var fileStream = new FileStream(file, FileMode.Create))
+        {
+            await Upload.CopyToAsync(fileStream);
+        }
     }
 }
