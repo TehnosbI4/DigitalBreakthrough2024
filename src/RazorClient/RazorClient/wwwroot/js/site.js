615aa14724ad57d1fb177725650d3223c144b1d6
vinni-sa <sandra.agapova@gmail.com>
2024-05-18 14:29:54 +0400

    add file uploaded

--- src/RazorClient/RazorClient/wwwroot/js/site.js
+++ src/RazorClient/RazorClient/wwwroot/js/site.js
@@ -2,3 +2,14 @@
 // for details on configuring this project to bundle and minify static web assets.
 
 // Write your JavaScript code.
+
+import * as FilePond from 'filepond';
+
+// Create a multi file upload component
+const pond = FilePond.create({
+    multiple: true,
+    name: 'filepond'
+});
+
+// Add it to the DOM
+document.body.appendChild(pond.element);
