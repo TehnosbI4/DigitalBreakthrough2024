bf2eb6c53a8e410d705d2abc64a4f4852581a7d6
Amir <amirmahdi13579@gmail.com>
2024-05-18 09:55:46 +0300

    добавил мультиселект

--- src/RazorClient/RazorClient/Pages/Index.cshtml.cs
+++ src/RazorClient/RazorClient/Pages/Index.cshtml.cs
@@ -1,32 +1,26 @@
+using System.ComponentModel.DataAnnotations;
+using System.Net.Http.Headers;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
-using IHostingEnvironment = Microsoft.AspNetCore.Hosting.IHostingEnvironment;
 
 namespace RazorClient.Pages;
 
 public class IndexModel : PageModel
 {
     private readonly ILogger<IndexModel> _logger;
-    private IHostingEnvironment _environment;
+    private IWebHostEnvironment _environment;
 
-    public IndexModel(ILogger<IndexModel> logger, IHostingEnvironment hostingEnvironment)
+    public IndexModel(ILogger<IndexModel> logger, IWebHostEnvironment hostingEnvironment)
     {
         _logger = logger;
         _environment = hostingEnvironment;
     }
 
-    public void OnGet()
-    {
-    }
+    [BindProperty, Display(Name="File")]
+    public IList<IFormFile> UploadedFiles { get; set; }
     
-    [BindProperty]
-    public IFormFile Upload { get; set; }
-    public async Task OnPostAsync()
+    public void OnPost()
     {
-        var file = Path.Combine(_environment.ContentRootPath, "uploads", Upload.FileName);
-        using (var fileStream = new FileStream(file, FileMode.Create))
-        {
-            await Upload.CopyToAsync(fileStream);
-        }
+        var e = UploadedFiles;
     }
 }
