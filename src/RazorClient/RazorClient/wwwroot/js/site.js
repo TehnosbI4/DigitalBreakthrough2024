2f31e7236a07a2e718bb07767fdc115fba059df4
vinni-sa <sandra.agapova@gmail.com>
2024-05-18 17:38:16 +0400

    изменен файл site.js и index

--- src/RazorClient/RazorClient/wwwroot/js/site.js
+++ src/RazorClient/RazorClient/wwwroot/js/site.js
@@ -1,15 +1,4 @@
 ﻿// Please see documentation at https://learn.microsoft.com/aspnet/core/client-side/bundling-and-minification
 // for details on configuring this project to bundle and minify static web assets.
 
-// Write your JavaScript code.
 
-import * as FilePond from 'filepond';
-
-// Create a multi file upload component
-const pond = FilePond.create({
-    multiple: true,
-    name: 'filepond'
-});
-
-// Add it to the DOM
-document.body.appendChild(pond.element);
