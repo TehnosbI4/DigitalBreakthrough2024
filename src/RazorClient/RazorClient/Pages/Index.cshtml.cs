39de15f53e9d128cbd2052fe0f7057548b41894c
Amir <amirmahdi13579@gmail.com>
2024-05-18 16:01:47 +0300

    добавлен http request

--- src/RazorClient/RazorClient/Pages/Index.cshtml.cs
+++ src/RazorClient/RazorClient/Pages/Index.cshtml.cs
@@ -9,6 +9,7 @@ public class IndexModel : PageModel
 {
     private readonly ILogger<IndexModel> _logger;
     private IWebHostEnvironment _environment;
+    private static HttpClient _httpClient = new();
 
     public IndexModel(ILogger<IndexModel> logger, IWebHostEnvironment hostingEnvironment)
     {
@@ -19,8 +20,22 @@ public class IndexModel : PageModel
     [BindProperty, Display(Name="File")]
     public IList<IFormFile> UploadedFiles { get; set; }
     
-    public void OnPost()
+    public async Task OnPost()
     {
-        var e = UploadedFiles;
+        foreach (var file in UploadedFiles)
+        {
+            using var stream = new MemoryStream((int)file.Length);
+            await file.CopyToAsync(stream);
+            var bytes=stream.ToArray();
+            await SendMp3Async(bytes);
+        }
+    }
+
+    private static async Task SendMp3Async(byte[] bytes)
+    {
+        var content = new ByteArrayContent(bytes);
+        using var response = await _httpClient.PostAsync("https://localhost:5000/", content);
+        var responseText = await response.Content.ReadAsStringAsync();
+        Console.WriteLine(responseText);
     }
 }
