97dc85ed3b122c709404d355bed805eb47ac8bb5
Amir <amirmahdi13579@gmail.com>
2024-05-18 20:34:13 +0300

    Сохранение файлов на диск

--- src/RazorClient/RazorClient/Pages/Index.cshtml.cs
+++ src/RazorClient/RazorClient/Pages/Index.cshtml.cs
@@ -11,16 +11,17 @@ public class IndexModel : PageModel
     private readonly ILogger<IndexModel> _logger;
     private IWebHostEnvironment _environment;
     private static HttpClient _httpClient = new();
+    private string _filesPath;
 
     public IndexModel(ILogger<IndexModel> logger, IWebHostEnvironment hostingEnvironment)
     {
         _logger = logger;
         _environment = hostingEnvironment;
+        _filesPath = Path.Combine(_environment.ContentRootPath, "AudioFiles");
     }
 
-    [BindProperty, Display(Name="File")]
-    public IList<IFormFile> UploadedFiles { get; set; }
-    
+    [BindProperty, Display(Name = "File")] public IList<IFormFile> UploadedFiles { get; set; }
+
     public async Task OnPost()
     {
         var audioFiles = await GetAudioFilesAsync();
@@ -30,15 +31,26 @@ public class IndexModel : PageModel
 
     private async Task<List<AudioFile>> GetAudioFilesAsync()
     {
+        var exists = Directory.Exists(_filesPath);
+
+        if (!exists)
+        {
+            Directory.CreateDirectory(_filesPath);
+        }
+
         var audioFiles = new List<AudioFile>();
         foreach (var file in UploadedFiles)
         {
             using var stream = new MemoryStream((int)file.Length);
             await file.CopyToAsync(stream);
-            var bytes=stream.ToArray();
+            var bytes = stream.ToArray();
             audioFiles.Add(new AudioFile(file.FileName, bytes));
+
+            var filePath = Path.Combine(_filesPath, file.FileName);
+            await using var fileStream = new FileStream(filePath, FileMode.Create);
+            await file.CopyToAsync(fileStream);
         }
-        
+
         return audioFiles;
     }
 
@@ -60,7 +72,7 @@ public class IndexModel : PageModel
 
         var jsonResponse = await response.Content.ReadAsStringAsync();
         Console.WriteLine($"{jsonResponse}\n");
-        
+
         return jsonResponse;
     }
 }
